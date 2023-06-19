90f6f12dbfa6541e9dba32798d3d650bfb86c556
BSMiguell <bruno.s.miiguel@gmail.com>
2023-06-19 02:43:40 -0300

    Criando a NavBar.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,66 @@
 <template>
-  <hello-world />
+  <v-app>
+    <NavBar />
+    <FooterView />
+    <v-container fluid></v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import NavBar from "../components/NavBar";
+import FooterView from "../components/FooterView";
 
 export default {
   name: "HomeView",
 
   components: {
-    HelloWorld,
+    NavBar,
+    FooterView,
   },
 };
 </script>
+
+<style>
+.marginLeft {
+  margin-left: -90px;
+}
+.mtop {
+  margin-top: 100px;
+}
+.mbottom {
+  margin-bottom: 100px;
+}
+.v-card.borderme {
+  border: 2px solid green !important;
+}
+.v-card.borderout {
+  border: 1px solid #d5f0db !important;
+}
+.v-btn:not(.v-btn--round).v-size--default.add {
+  min-width: 0px !important;
+}
+.theme--light.v-sheet--outlined.mobile {
+  border: 2px solid black !important;
+}
+@media only screen and (max-width: 600px) {
+  h2.title1 {
+    font-size: 15px;
+  }
+  h2.title2 {
+    font-size: 15px;
+  }
+  .top {
+    margin-top: 20px;
+  }
+}
+@media only screen and (min-width: 600px) {
+  .top {
+    margin-top: 70px;
+  }
+}
+@media only screen and (min-width: 768px) {
+  .top {
+    margin-top: 120px;
+  }
+}
+</style>
